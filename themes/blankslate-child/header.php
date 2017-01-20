78c6d77c9509ce93efab107281cc23e7085954f1
Thomas Giraud <Giraud@MacBook-Air-de-Thomas.local>
2017-01-20 14:55:02 +0100

    Mod

--- themes/blankslate-child/header.php
+++ themes/blankslate-child/header.php
@@ -11,7 +11,7 @@
   <header id="header" role="banner">
     <section id="branding">
         <input class='bouton' type=button onclick=window.location.href='http://monsite.fr'; value='Référencer ma salle' />
-      <div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
+    <!--  <div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div> -->
     </section>
   </header>
 <div id="container">
