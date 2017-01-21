28ac06d29f463fa148cccfad35885a06a3c97b02
Wickedpool <thgiraud@student.42.fr>
2017-01-21 15:42:59 +0100

    AddMetaWooCom

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -7,3 +7,22 @@ wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri(). '/bootstrap/js
 wp_enqueue_script('perso-js', get_stylesheet_directory_uri(). '/scripts/scripts.js');
 }
 add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles');
+
+function msk_add_love_product_tab($tabs) {
+	
+	$tabs['love_tab'] = array(
+		'title' 	=> __('Popularity', 'msk'),
+		'priority' 	=> 15,
+		'callback' 	=> 'msk_add_love_product_tab_content'
+	);
+
+	return $tabs;
+
+}
+add_filter('woocommerce_product_tabs', 'msk_add_love_product_tab');
+
+function msk_add_love_product_tab_content() {
+	wc_get_template('single-product/tabs/love-product.php');
+}
+
+?>
