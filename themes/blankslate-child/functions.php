af3e177ec2dd2a51074d1383a380783934b2796f
user <user@MacBook-Pro-de-user-3.local>
2017-01-23 11:59:28 +0100

    changes on front

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 function wpm_enqueue_styles(){
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( 'parent-style', get_stylesheet_directory_uri() . '/css/style.css' );
 wp_enqueue_style('bootstrap-css', get_stylesheet_directory_uri(). '/bootstrap/css/bootstrap.min.css');
 wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri(). '/scripts/jquery.js');
 wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri(). '/bootstrap/js/bootstrap.min.js');
