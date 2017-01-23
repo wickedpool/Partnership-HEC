f1abcf26dea76a90d93fd0291c2a3cff0af8c664
user <user@MacBook-Pro-de-user-3.local>
2017-01-23 17:43:24 +0100

    searchbar in progress

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -7,3 +7,15 @@ wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri(). '/bootstrap/js
 wp_enqueue_script('perso-js', get_stylesheet_directory_uri(). '/scripts/scripts.js');
 }
 add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles');
+?>
+
+<?php
+add_filter( 'query_vars', 'willy_add_query_vars' );
+function willy_add_query_vars( $vars ){
+	$vars[] = "ville";
+	$vars[] = "chambres";
+	$vars[] = "quartiers";
+	$vars[] = "prix-maxi";
+	$vars[] = "equipements";
+	return $vars;
+}
