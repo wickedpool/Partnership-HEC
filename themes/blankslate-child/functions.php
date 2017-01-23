ba60c471734b690525ce164ad43871d1f8583579
wickedpool <thgiraud@student.42.fr>
2017-01-23 11:36:44 +0100

    modifMeta

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -37,80 +37,10 @@ function msk_remove_additionalinfo_tab($tabs) {
 add_filter('woocommerce_product_tabs', 'msk_remove_additionalinfo_tab', 10);
 
 /*************************************************************************************************
-* On ajoute des champs de test pour se repérer, dans l'onglet Général
-
-function msk_add_test_field_data() {
-	echo '<div style="background:#f8fbca; padding:1em;">';
-
-	echo '<h4>Testons les différents types de champs</h4>';
-
-	// Champ de type text
-	woocommerce_wp_text_input(
-		array(
-			'id' => 'input_text', 
-			'label' => __('Champ de type "text"', 'msk'),
-			'placeholder' => __('Placeholder du champ text', 'msk'),
-			'description' => __('La description peut apparaître dans une infobulle si "desc_tip" est sur "true".', 'msk'),
-			'desc_tip' => true // Si "true", la description s'affichera en infobulle
-		)
-	);
-
-	// Champ de type hidden
-	woocommerce_wp_hidden_input(
-		array(
-			'id' => 'input_hidden', 
-			'label' => __('Champ de type "hidden"', 'msk'),
-			'value' => 'valeur-du-champ-hidden',
-		)
-	);
-
-	// Champ de type textarea
-	woocommerce_wp_textarea_input(
-		array(
-			'id' => 'input_textarea', 
-			'label' => __('Champ de type "textarea"', 'msk'),
-			'class' => 'widefat',
-			'placeholder' => __('Placeholder du champ textarea', 'msk'),
-			'description' => __('<br>La description n\'apparaîtra pas dans une infobulle si "desc_tip" est sur "false" ou inexistant.', 'msk'),
-			'custom_attributes' => array( // Un tableau d'attributs personnalisés qui seront ajoutés au champ en question
-				'data-test' => 50,
-				'data-other-test' => 'Lorem ipsum'
-			)
-		)
-	);
-
-	// Champ de type checkbox
-	woocommerce_wp_checkbox(
-		array(
-			'id' => 'input_checkbox', 
-			'label' => __('Champ de type "checkbox"', 'msk'),
-			'value' => 'yes', // La valeur enregistrée de la checkbox
-			'cbvalue' => 'yes',  // La valeur de la checkbox en question. Si elle est la même que "value", la checkbox sera cochée
-		)
-	);
-
-	// Champ de type select
-	woocommerce_wp_select(
-		array(
-			'id' => 'input_select', 
-			'label' => __('Champ de type "select"', 'msk'),
-			'options' => array( // Un tableau avec les options (value et nom) du select
-				'value1' => 'Am',
-				'value2' => 'Stram',
-				'value3' => 'Gram',
-			),
-			'value' => 'value2', // La valeur enregistrée, choisie, de la select
-		)
-	);
-}
-add_action('woocommerce_product_options_general_product_data', 'msk_add_test_field_data'); 
-
-********************************************************************************************
-*******************************************************************************************/
-
-/*************************************************************************************************
 * On ajoute 2 champs (post meta ou custom field) aux produits WC dans l'onglet "Avancé"
+
 *************************************************************************************************/
+
 function msk_add_loves_hates_fields_to_product() {
 	woocommerce_wp_text_input(
 		array(
@@ -209,17 +139,6 @@ woocommerce_wp_text_input(
 	)
 );
 
-woocommerce_wp_text_input(
-      array(
-        'id' => 'prix',
-        'data_type' => 'decimal',
-        'label' => __('prix', 'msk'),
-        'placeholder' => __('Entrez le prix indicatif de la salle', 'msk'),
-        'description' => __('Hatred this product has received.', 'msk'),
-		'desc_tip' => true
-	)
-);
-
 	woocommerce_wp_radio(
 		array(
 			'id' => 'reponse', 
@@ -259,11 +178,6 @@ function msk_save_hotel_fields($product_id, $post, $update) {
   if (isset($_POST['capacite'])) {
       $hates = $_POST['capacite'];
       update_post_meta($product_id, 'capacite', $hates);
-    }
-
-  if (isset($_POST['prix'])) {
-      $hates = $_POST['prix'];
-      update_post_meta($product_id, 'prix', $hates);
     }
 
   if (isset($_POST['reponse'])) {
